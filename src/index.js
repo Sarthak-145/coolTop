6ff8270534ad6f434f862bbb84cbd419ea30d26e
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-08-01 19:54:08 +0530

    write equivalent logic to make processParser work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { getCpu } from './proc/cpu.js';
 import { getProcesses } from './proc/process.js';
+import parseCpuData from './parser/cpustatParser.js';
 
-// const cpuData = await getCpu();
+const cpuData = await getCpu();
+parseCpuData(cpuData);
+
 // console.log('CPU DATA: ', cpuData);
 
-const rawProcessFiles = await getProcesses();
+// const rawProcessFiles = await getProcesses();
