f0aed8fb098c45ba9bee43603b42e3b698c1e280
Sarthak-145 <sarthakkanawade145@gmail.com>
2026-08-01 19:53:08 +0530

    write parser for 16 cores of cpus, rest of the /stat is still not parsed

---
+++ src/parser/cpustatParser.js
@@ -0,0 +1,41 @@
+// parses data from /proc/stat
+const parseCpuData = (data) => {
+  const lines = data.toString().trim().split('\n');
+
+  // for 16 core cpu
+  const cpus = {};
+  for (let i = 0; i < 16; i++) {
+    // console.log(lines[i]);
+    const line = lines[i].trim().split(/\s+/);
+    const [
+      name,
+      user,
+      nice,
+      system,
+      idle,
+      iowait,
+      irq,
+      softirq,
+      steal,
+      guest,
+      guestNice,
+    ] = line;
+    cpus[name] = {
+      user: Number(user),
+      nice: Number(nice),
+      system: Number(system),
+      idle: Number(idle),
+      iowait: Number(iowait),
+      irq: Number(irq),
+      softirq: Number(softirq),
+      steal: Number(steal),
+      guest: Number(guest),
+      guestNice: Number(guestNice),
+    };
+  }
+  console.log(cpus);
+};
+
+const singleCpu = () => {};
+
+export default parseCpuData;
